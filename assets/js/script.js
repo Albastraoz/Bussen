eca9cc74e6aa8cf303b666ff17c0a28332ef27e3
Ubuntu <ubuntu@ip-172-31-91-138.ec2.internal>
2019-11-13 14:47:49 +0000

    Version 1.02 - Added all cards images and show the randomized card to webpage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-    var cardDeck = ['sa', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 'sj', 'sq', 'sk', 'da', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'dj', 'dq', 'dk', 'ca', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'cj', 'cq', 'ck', 'ha', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'hj', 'hq', 'hk', ];
+    var cardDeck = ['sa', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 'sj', 'sq', 'sk', 'da', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'dj', 'dq', 'dk', 'ca', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'cj', 'cq', 'ck', 'ha', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'hj', 'hq', 'hk', ];
     var randomCard1;
     var randomCard2;
     var randomCard3;
@@ -26,21 +26,21 @@
         randomCard8 = pickRandomCard();
         randomCard9 = pickRandomCard();
         randomCard10 = pickRandomCard();
-        return $('#card1').append(randomCard1),
-            $('#card2').append(randomCard2),
-            $('#card3').append(randomCard3),
-            $('#card4').append(randomCard4),
-            $('#card5').append(randomCard5),
-            $('#card6').append(randomCard6),
-            $('#card7').append(randomCard7),
-            $('#card8').append(randomCard8),
-            $('#card9').append(randomCard9),
-            $('#card10').append(randomCard10),
+        return $('#card1').append(`<img class="card_style" src="assets/images/cards/${randomCard1}.png"></img>`),
+            $('#card2').append(`<img class="card_style" src="assets/images/cards/${randomCard2}.png"></img>`),
+            $('#card3').append(`<img class="card_style" src="assets/images/cards/${randomCard3}.png"></img>`),
+            $('#card4').append(`<img class="card_style" src="assets/images/cards/${randomCard4}.png"></img>`),
+            $('#card5').append(`<img class="card_style" src="assets/images/cards/${randomCard5}.png"></img>`),
+            $('#card6').append(`<img class="card_style" src="assets/images/cards/${randomCard6}.png"></img>`),
+            $('#card7').append(`<img class="card_style" src="assets/images/cards/${randomCard7}.png"></img>`),
+            $('#card8').append(`<img class="card_style" src="assets/images/cards/${randomCard8}.png"></img>`),
+            $('#card9').append(`<img class="card_style" src="assets/images/cards/${randomCard9}.png"></img>`),
+            $('#card10').append(`<img class="card_style" src="assets/images/cards/${randomCard10}.png"></img>`),
             console.log(cardDeck);
     }
 
     function resetCardDeck() {
-        cardDeck = ['sa', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 'sj', 'sq', 'sk', 'da', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'dj', 'dq', 'dk', 'ca', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'cj', 'cq', 'ck', 'ha', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'hj', 'hq', 'hk', ];
+        cardDeck = ['sa', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 'sj', 'sq', 'sk', 'da', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'dj', 'dq', 'dk', 'ca', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'cj', 'cq', 'ck', 'ha', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'hj', 'hq', 'hk', ];
         randomCard1 = [];
         randomCard2 = [];
         randomCard3 = [];
@@ -61,17 +61,7 @@
         $('#card8').empty();
         $('#card9').empty();
         $('#card10').empty();
-        return console.log(randomCard1),
-            console.log(randomCard2),
-            console.log(randomCard3),
-            console.log(randomCard4),
-            console.log(randomCard5),
-            console.log(randomCard6),
-            console.log(randomCard7),
-            console.log(randomCard8),
-            console.log(randomCard9),
-            console.log(randomCard10),
-            console.log(cardDeck);
+        return console.log(cardDeck);
     }
 
 	$("#dealButton").on("click", function() {
