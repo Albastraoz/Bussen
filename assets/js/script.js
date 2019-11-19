4763ac073e6fb4a7b30c28e28a6a1f5ea5010843
Robin Kaal <rkaal7@gmail.com>
2019-11-19 14:38:29 +0100

    Version 1.15 - Bug fixes and revamping code to be shorter and better.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,9 @@ var yourChoice = 'black';
 var correctAnswer;
 
 var cardIdentifier;
-var loopIdentifier;
 var roundIdentifier;
 var switchIdentifier;
 var endLoopIdentifier;
-
-var round1;
-var round2;
-var round3;
-var round4;
 
 var roundRule;
 var rule1 = 'Take 1 zip';
@@ -32,16 +26,6 @@ var randomCards = new Array();
 
 var dealCards = new Audio("assets/sounds/deal_cards.wav");
 var flipCard = new Audio("assets/sounds/flip_card.wav");
-
-function checkCardColor() {
-    for (i = 0; i < 26; i++) {
-        if (randomCards[cardIdentifier].indexOf(redCards[i]) > -1) {
-            correctAnswer = `red`;
-        } else if (randomCards[cardIdentifier].indexOf(blackCards[i]) > -1) {
-            correctAnswer = `black`;
-        }
-    }
-}
 
 function changeColor() {
     if ($('#color_choice').is(':checked')) {
@@ -66,18 +50,22 @@ function startGame() {
 
     for (i = randomCards.length; i < 10; i++) {
         randomCards.push(cardDeck.splice(Math.floor(Math.random() * cardDeck.length), 1));
+        console.log(randomCards.length);
     }
 
-    for (i = 1; i < 11; i++) {
-        if (i > 4) {
+    for (i = 0; i < 10; i++) {
+        if (i > 3) {
             $('#btn_card' + i).prop('disabled', true);
             $('#card' + i).append(`<img class="card_style" src="assets/images/card_covers/card_cover_default_disabled.png"></img>`);
-        } else if (i <= 4) {
+        } else if (i <= 3) {
             $('#card' + i).append(`<img class="card_style" src="assets/images/card_covers/card_cover_default.png"></img>`);
             $('#btn_card' + i).prop('disabled', false);
         }
     }
     $('#warning_textfield').append(`<p>Good luck ${playerName}!</p>`);
+
+    console.log(randomCards.toString());
+    console.log(`-- Create random Cards --`);
 }
 
 $('#resetButton').click(function () {
@@ -85,106 +73,105 @@ $('#resetButton').click(function () {
     $('#warning_textfield').empty();
     window.scrollTo({ top: 0, behavior: 'smooth' });
 
-    for (i = randomCards.length; i < 10; i++) {
-        if (cardDeck.length < 1) {
-            for (j = 0; j < playedCards.length; j++) {
-                cardDeck.push(playedCards[t]);
-            }
-            playedCards = [];
-        }
-        randomCards.push(cardDeck.splice(Math.floor(Math.random() * cardDeck.length), 1));
-    }
-
-    for (i = 1; i < 11; i++) {
+    for (i = 0; i < 10; i++) {
         $('#card' + i).empty();
     }
 
-    for (i = 1; i < 11; i++) {
-        if (i > 4) {
+    for (i = 0; i < 10; i++) {
+        if (i > 3) {
             $('#btn_card' + i).prop('disabled', true);
             $('#card' + i).append(`<img class="card_style" src="assets/images/card_covers/card_cover_default_disabled.png"></img>`);
-        } else if (i <= 4) {
+        } else if (i <= 3) {
             $('#card' + i).append(`<img class="card_style" src="assets/images/card_covers/card_cover_default.png"></img>`);
             $('#btn_card' + i).prop('disabled', false);
         }
     }
+    console.log(randomCards.toString());
+    console.log(`-- RESET CLICKED --`);
 });
 
-$('#btn_card1, #btn_card2, #btn_card3, #btn_card4, #btn_card5, #btn_card6, #btn_card7, #btn_card8, #btn_card9').click(function () {
+$('#btn_card0, #btn_card1, #btn_card2, #btn_card3, #btn_card4, #btn_card5, #btn_card6, #btn_card7, #btn_card8, #btn_card9').click(function () {
     flipCard.play();
     $('#warning_textfield').empty();
 
-    if (this.id == 'btn_card1') {
+    console.log(randomCards.toString());
+    console.log(randomCards.length);
+    console.log(`First Check`);
+
+    if (this.id == 'btn_card0') {
         cardIdentifier = 0;
-        loopIdentifier = cardIdentifier + 1;
-        roundIdentifier = 1;
-        switchIdentifier = 4;
-        endLoopIdentifier = 8;
+        roundIdentifier = 0;
+        switchIdentifier = 3;
+        endLoopIdentifier = 7;
+        roundRule = rule1;
+    } else if (this.id == 'btn_card1') {
+        cardIdentifier = 1;
+        roundIdentifier = 0;
+        switchIdentifier = 3;
+        endLoopIdentifier = 7;
         roundRule = rule1;
     } else if (this.id == 'btn_card2') {
-        cardIdentifier = 1;
-        loopIdentifier = cardIdentifier + 1;
-        roundIdentifier = 1;
-        switchIdentifier = 4;
-        endLoopIdentifier = 8;
+        cardIdentifier = 2;
+        roundIdentifier = 0;
+        switchIdentifier = 3;
+        endLoopIdentifier = 7;
         roundRule = rule1;
     } else if (this.id == 'btn_card3') {
-        cardIdentifier = 2;
-        loopIdentifier = cardIdentifier + 1;
-        roundIdentifier = 1;
-        switchIdentifier = 4;
-        endLoopIdentifier = 8;
+        cardIdentifier = 3;
+        roundIdentifier = 0;
+        switchIdentifier = 3;
+        endLoopIdentifier = 7;
         roundRule = rule1;
     } else if (this.id == 'btn_card4') {
-        cardIdentifier = 3;
-        loopIdentifier = cardIdentifier + 1;
-        roundIdentifier = 1;
-        switchIdentifier = 4;
-        endLoopIdentifier = 8;
-        roundRule = rule1;
+        cardIdentifier = 4;
+        roundIdentifier = 4;
+        switchIdentifier = 6;
+        endLoopIdentifier = 9;
+        roundRule = rule2;
+        window.scrollTo({ top: 100, behavior: 'smooth' });
     } else if (this.id == 'btn_card5') {
-        cardIdentifier = 3;
-        loopIdentifier = cardIdentifier + 2;
-        roundIdentifier = 5;
-        switchIdentifier = 7;
-        endLoopIdentifier = 10;
+        cardIdentifier = 5;
+        roundIdentifier = 4;
+        switchIdentifier = 6;
+        endLoopIdentifier = 9;
         roundRule = rule2;
         window.scrollTo({ top: 100, behavior: 'smooth' });
     } else if (this.id == 'btn_card6') {
-        cardIdentifier = 4;
-        loopIdentifier = cardIdentifier + 2;
-        roundIdentifier = 5;
-        switchIdentifier = 7;
-        endLoopIdentifier = 10;
+        cardIdentifier = 6;
+        roundIdentifier = 4;
+        switchIdentifier = 6;
+        endLoopIdentifier = 9;
         roundRule = rule2;
         window.scrollTo({ top: 100, behavior: 'smooth' });
     } else if (this.id == 'btn_card7') {
-        cardIdentifier = 5;
-        loopIdentifier = cardIdentifier + 2;
-        roundIdentifier = 5;
-        switchIdentifier = 7;
+        cardIdentifier = 7;
+        roundIdentifier = 7;
+        switchIdentifier = 8;
         endLoopIdentifier = 10;
-        roundRule = rule2;
-        window.scrollTo({ top: 100, behavior: 'smooth' });
+        roundRule = rule3;
+        window.scrollTo({ top: 380, behavior: 'smooth' });
     } else if (this.id == 'btn_card8') {
-        cardIdentifier = 5;
-        loopIdentifier = cardIdentifier + 3;
-        roundIdentifier = 8;
-        switchIdentifier = 9;
-        endLoopIdentifier = 11;
+        cardIdentifier = 8;
+        roundIdentifier = 7;
+        switchIdentifier = 8;
+        endLoopIdentifier = 10;
         roundRule = rule3;
         window.scrollTo({ top: 380, behavior: 'smooth' });
     } else if (this.id == 'btn_card9') {
-        cardIdentifier = 6;
-        loopIdentifier = cardIdentifier + 3;
-        roundIdentifier = 8;
+        cardIdentifier = 9;
+        roundIdentifier = 9;
         switchIdentifier = 9;
-        endLoopIdentifier = 11;
-        roundRule = rule3;
-        window.scrollTo({ top: 380, behavior: 'smooth' });
+        endLoopIdentifier = 10;
+        roundRule = rule4;
     }
 
-    checkCardColor();
+    for (i = 0; i < 26; i++) {
+        if (randomCards[cardIdentifier].indexOf(redCards[i]) > -1) {
+            correctAnswer = `red`;
+        } else if (randomCards[cardIdentifier].indexOf(blackCards[i]) > -1) {
+            correctAnswer = `black`;
+        }
+    }
 
     if (yourChoice === correctAnswer) {
         for (i = roundIdentifier; i < endLoopIdentifier; i++) {
@@ -194,59 +181,51 @@ $('#btn_card1, #btn_card2, #btn_card3, #btn_card4, #btn_card5, #btn_card6, #btn_
                 $('#btn_card' + i).prop('disabled', false);
             } else if (i <= switchIdentifier) {
                 $('#btn_card' + i).prop('disabled', true);
-                if (i == loopIdentifier) {
+                if (i == cardIdentifier) {
                     $('#card' + i).append(`<img class="card_style" src="assets/images/cards/${randomCards[cardIdentifier]}.png"></img>`);
                 } else {
                     $('#card' + i).append(`<img class="card_style" src="assets/images/card_covers/card_cover_default_disabled.png"></img>`);
                 }
             }
         }
-        $('#warning_textfield').append('<p>You were CORRECT! Choose your next card.</p>');
+        if (cardIdentifier == 9) {
+            succeedAmount = succeedAmount + 1;
+            $('#warning_textfield').append('<p>Congratulations! You WON the game and may leave the buss!<br> Choose the next person to enter the buss.</p>');
+        } else {
+            $('#warning_textfield').append('<p>You were CORRECT! Choose your next card.</p>');
+        }
     } else {
         for (i = roundIdentifier; i < 11; i++) {
             $('#card' + i).empty();
-            if (i != loopIdentifier) {
+            if (i != cardIdentifier) {
                 $('#card' + i).append(`<img class="card_style" src="assets/images/card_covers/card_cover_default_disabled.png"></img>`);
             }
             $('#btn_card' + i).prop('disabled', true);
         }
-        $('#card' + loopIdentifier).append(`<img class="card_style" src="assets/images/cards/${randomCards[cardIdentifier]}.png"></img>`);
+        $('#card' + cardIdentifier).append(`<img class="card_style" src="assets/images/cards/${randomCards[cardIdentifier]}.png"></img>`);
         attemptAmount = attemptAmount + 1;
         $('#warning_textfield').append(`<p>You were wrong! ${roundRule} and try again.</p>`);
     }
 
     $('#showStats').empty();
     $('#showStats').append(`<p>Attempts: ${attemptAmount}<br>Wins: ${succeedAmount}</p>`);
-    
+    console.log(randomCards.toString());
+    console.log(randomCards.length);
+    console.log(`Before New Card`);
     playedCards.push(randomCards[cardIdentifier]);
     randomCards.splice(cardIdentifier, 1);
-    randomCards.splice(cardIdentifier, 0, cardDeck.splice(Math.floor(Math.random() * cardDeck.length), 1));
-});
-
-$('#btn_card10').click(function () {
-    flipCard.play();
-    $('#warning_textfield').empty();
-
-    cardIdentifier = 6;
-    roundRule = rule4;
-
-    checkCardColor();
-
-    if (yourChoice === correctAnswer) {
-        $('#card10').empty();
-        $('#card10').append(`<img class="card_style" src="assets/images/cards/${randomCards[cardIdentifier]}.png"></img>`);
-        succeedAmount = succeedAmount + 1;
-        $('#warning_textfield').append('<p>Congratulations! You WON the game and may leave the buss!<br> Choose the next person to enter the buss.</p>');
-    } else {
-        $('#card10').empty();
-        $('#card10').append(`<img class="card_style" src="assets/images/cards/${randomCards[cardIdentifier]}.png"></img>`);
-        attemptAmount = attemptAmount + 1;
-        $('#warning_textfield').append(`<p>You were wrong! ${roundRule} and try again.</p>`);
+    console.log(randomCards.toString());
+    console.log(randomCards.length);
+    console.log(`Take out Card`);
+    if (cardDeck.length < 1) {
+        for (i = 0; i < playedCards.length; i++) {
+            cardDeck.push(playedCards[i]);
+        }
+        playedCards = [];
     }
-
-    $('#showStats').empty();
-    $('#showStats').append(`<p>Attempts: ${attemptAmount}<br>Wins: ${succeedAmount}</p>`);
-
-    playedCards.push(randomCards[cardIdentifier]);
-    randomCards.splice(6, 1);
+    randomCards.splice(cardIdentifier, 0, cardDeck.splice(Math.floor(Math.random() * cardDeck.length), 1));
+    console.log(randomCards.toString());
+    console.log(randomCards.length);
+    console.log(`Push in new card`);
+    console.log(`-------------------------`);
 });
